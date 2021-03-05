ab9382532ba0405b79705010c2032bc191839b40
alberto <alberto.valenti@varprime.com>
2021-03-05 10:53:01 +0100

    css

--- web/web_script.js
+++ web/web_script.js
@@ -4,7 +4,7 @@ function CallJavaScript(s) {
     Microsoft.Dynamics.NAV.InvokeExtensibilityMethod('OnBcPageCallBack', [s]);
 }
 
-function RaiseControlAddInEvent(index,message) {
-    Microsoft.Dynamics.NAV.InvokeExtensibilityMethod('OnBcRaiseControlAddInEventCallBack', [index,message]);
+function RaiseControlAddInEvent(index, message) {
+    Microsoft.Dynamics.NAV.InvokeExtensibilityMethod('OnBcRaiseControlAddInEventCallBack', [index, message]);
 }
 
